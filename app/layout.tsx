ac22b16d6577b5f70cec17c0277644cc417c7493
aryankit-Block <aryankitb@gmail.com>
2025-04-24 14:06:33 +0530

    Update favicon configuration and add new logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,14 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aryankit's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Aryankit | Portfolio",
+  description: "Modern & Minimal Portfolio",
+  icons: {
+    icon: "/aryankit.logo.png",
+    shortcut: "/aryankit.logo.png",
+    apple: "/aryankit.logo.png"
+  },
+  manifest: "/manifest.json"
 };
 
 export default function RootLayout({
@@ -20,7 +26,17 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link 
+          rel="icon" 
+          type="image/png" 
+          href="/aryankit.logo.png"
+          sizes="64x64"
+        />
+        <link 
+          rel="apple-touch-icon" 
+          href="/aryankit.logo.png"
+          sizes="180x180"
+        />
       </head>
       <body className={inter.className}>
         <ThemeProvider
